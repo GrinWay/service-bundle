e565bb94d4c24c47fac9a188c2af79d04ab94165
Grigory Koblitskiy <grin180898@outlook.com>
2025-02-04 13:30:38 +0300

    add(test trait)

---
+++ src/Test/Trait/HasBufferTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GrinWay\Service\Test\Trait;
+
+trait HasBufferTest
+{
+    protected static function assertOutputBufferWasNotUsed(): void
+    {
+        $bufferListStatus = \ob_get_status(full_status: true);
+        foreach ($bufferListStatus as $bufferStatus) {
+            static::assertSame(
+                0,
+                $bufferStatus['buffer_used'] ?? 0,
+                message: 'Buffer was not used',
+            );
+        }
+    }
+}
