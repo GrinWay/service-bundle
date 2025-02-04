2d483ef05e01046ecfe682a51a3ba8c2797098e1
Grigory Koblitskiy <grin180898@outlook.com>
2025-02-04 14:45:52 +0300

    add(assertOutputBufferWasNotUsed argument dop message to dump)

--- src/Test/Trait/HasBufferTest.php
+++ src/Test/Trait/HasBufferTest.php
@@ -4,14 +4,16 @@ namespace GrinWay\Service\Test\Trait;
 
 trait HasBufferTest
 {
-    protected static function assertOutputBufferWasNotUsed(): void
+    protected static function assertOutputBufferWasNotUsed(?string $dopMessage = null): void
     {
+        $dopMessage ??= '';
+
         $bufferListStatus = \ob_get_status(full_status: true);
         foreach ($bufferListStatus as $bufferStatus) {
             static::assertSame(
                 0,
                 $bufferStatus['buffer_used'] ?? 0,
-                message: 'Output buffer was supposed not to be used',
+                message: \sprintf('Output buffer was supposed not to be used%s', $dopMessage),
             );
         }
     }
